a9565b856678c2a223604d2d140d357469286cec
Grace Erk <graceerk21@gmail.com>
2022-04-06 14:40:53 -0400

    Added a non-functional login button

--- src/FXMain.java
+++ src/FXMain.java
@@ -3,6 +3,7 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
@@ -15,6 +16,7 @@ public class FXMain extends Application {
     TextField userField;
     TextField passField;
     Label loginTitle;
+    Button loginBtn;
 
     public static void main(String[] args) {
         launch(args);
@@ -30,7 +32,7 @@ public class FXMain extends Application {
 
         loginPane.setMaxSize(400, 450);			//layout dimensions
         loginPane.setMinSize(400, 450);
-        loginPane.setVgap(10);
+        loginPane.setVgap(15);
         loginPane.setHgap(10);
 
         loginTitle = new Label("Log In");
@@ -40,12 +42,17 @@ public class FXMain extends Application {
         userField = new TextField("Username");
         passField = new TextField("Password");
 
+        loginBtn = new Button("LOG IN");
+        loginBtn.getStyleClass().clear();
+        loginBtn.getStyleClass().add("buttons");
+
         //index format is: (column, row, takes up x cols, takes up x rows)
         loginPane.add(loginTitle, 0, 0);
 
         //TODO: Connect fields to logic
         loginPane.add(userField, 0, 1);
         loginPane.add(passField, 0, 2);
+        loginPane.add(loginBtn, 0, 3);
         loginPane.setPadding(new Insets(20));
         loginPane.setAlignment(Pos.CENTER);
 
